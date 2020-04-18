f8446d6e8a0b214c00216bd87201cd27553f4111
xnot_found <63615338+xnot-found@users.noreply.github.com>
2020-04-18 22:13:22 +0700

    requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,29 @@
+#!/bin/python
+# 
+#
+# Name	:	IP Attack
+# Code		:	python
+# Sec.Code	:	fsociety alas roban
+# Coder		: 	xnot_found
+# Team		: 	FSOCIETY ALAS ROBAN
+# github	:	github.com/xnot-found
+#
+#
+#	<==(__WARNING__)==>
+#
+# DO NOT MODIFY OR COPY WITHOUT PERMISSION !!!
+#
+#	<==(__WARNING__)==>
+#
+#
+#
+apt update
+apt upgrade 
+apt install figlet -y
+apt install toilet -y
+apt install nano -y
+apt install ruby -y
+apt install curl -y
+gem install lolcat
+clear
+toilet -f pagga "Done..!!" | lolcat
